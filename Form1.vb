30ecf262a9bf61720edc3095042811e164587aa7
Lenovo-User <Lenovo-User@DESKTOP-T3K84PN>
2024-01-18 18:55:39 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("this is malaleng",, "Greeting") 'this is to make the lil box have a Hi instead of drawn words
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
